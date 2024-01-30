46b07bebe6f387a38df4536b27c1437d98b5b193
Akinterinwa Oluwanifemi <118985490+Akinterinwa@users.noreply.github.com>
2024-01-30 12:21:08 +0100

    additional work

--- lib/data.ts
+++ lib/data.ts
@@ -2,8 +2,8 @@ import React from "react";
 import { CgWorkAlt } from "react-icons/cg";
 import { FaReact } from "react-icons/fa";
 import { LuGraduationCap } from "react-icons/lu";
-import ecommerce from "@/public/corpcomment.png";
-import netflix from "@/public/rmtdev (1).png";
+import ecommerce from "../images/ecommerce.PNG";
+import netflix from "../images/netflix.PNG";
 import wordanalyticsImg from "@/public/wordanalytics.png";
 
 export const experiencesData = [
