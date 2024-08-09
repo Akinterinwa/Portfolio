e5de770c6320d9035dc1aaccb37f362226aa57f8
Akinterinwa Oluwanifemi <118985490+Akinterinwa@users.noreply.github.com>
2024-08-09 01:20:59 +0100

    just updated my resume on the portfolio

--- components/intro.tsx
+++ components/intro.tsx
@@ -82,7 +82,7 @@ export default function Intro() {
                 <a className='group bg-white px-7 py-3 flex items-center gap-2 rounded-full
                 outline-none focus:scale-110 borderBlack
                 hover:scale-110 active:scale-105 transition cursor-pointer dark:bg-white/10
-                ' href='/Akinterinwa-resume.pdf' download>Download CV <HiDownload className='
+                ' href='/Akinterinwa-new-resume.pdf' download>Download CV <HiDownload className='
                 opacity-60 group-hover:translate-y-1 transition'/>{" "}
                 </a>
 
